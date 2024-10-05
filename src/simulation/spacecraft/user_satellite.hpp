aed5ebae5d0bed70243285db2507e1af1b4e1f65
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2024-10-05 17:56:45 +0900

    Update for s2e v8

--- src/simulation/spacecraft/user_satellite.hpp
+++ src/simulation/spacecraft/user_satellite.hpp
@@ -22,8 +22,18 @@ class UserSatellite : public s2e::spacecraft::Spacecraft {
    */
   UserSatellite(const s2e::simulation::SimulationConfiguration *simulation_configuration,
                 const s2e::environment::GlobalEnvironment *global_environment, const unsigned int spacecraft_id);
+  /**
+   * @fn Update
+   * @brief Override Spacecraft::Update
+   */
+  virtual void Update(const s2e::environment::SimulationTime *simulation_time) override;
 
  private:
+  /**
+   * @fn ControlAlgorithm
+   * @brief Control algorithm
+   */
+  void ControlAlgorithm(void);
 };
 
 #endif  // S2E_SIMULATION_SPACECRAFT_USER_SATELLITE_HPP_
